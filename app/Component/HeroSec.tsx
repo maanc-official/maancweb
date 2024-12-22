850899ad244964425aea40fd639c05fc62dc0542
Mohamed Rizwan <mynameisrizwan35@gmail.com>
2024-12-23 00:54:13 +0530

    hero section alignment changes

--- app/Component/HeroSec.tsx
+++ app/Component/HeroSec.tsx
@@ -7,8 +7,9 @@ import Image from "next/image";
 const HeroSec = () => {
   return (
     <>
-      <div className="w-[95%] h-[100svh] mx-auto flex flex-col-reverse lg:flex-row items-center justify-between ">
-        {/* Text Section */}
+      {/* <div className="w-full h-[100svh] mx-auto flex flex-col-reverse lg:flex-row items-center justify-between bg-red-400">
+        
+       
         <div className="lg:w-1/2 flex flex-col items-center lg:items-start justify-center space-y-5 text-center lg:text-left">
           <div className="flex justify-center lg:justify-start space-x-1 text-[36px] md:text-[48px] lg:text-[58px] font-extrabold">
             <h1 className="text-btncolor">Maanc</h1>
@@ -38,7 +39,7 @@ const HeroSec = () => {
           </div>
         </div>
 
-        {/* Image Section */}
+      
         <div className="lg:w-1/2 flex items-center justify-center">
           <Image
             className="object-contain"
@@ -51,7 +52,52 @@ const HeroSec = () => {
                    250px" // Automatically adjust size
           />
         </div>
-      </div>
+      </div> */}
+
+<div className="flex h-[100svh] w-full flex-col justify-center px-28">
+  <div className="flex h-auto w-full flex-row justify-center items-center">
+    <div className="w-1/2 flex flex-col  items-start justify-center space-y-2">
+    <div className="flex justify-center lg:justify-start space-x-1 text-[36px] md:text-[48px] lg:text-[58px] font-extrabold">
+            <h1 className="text-btncolor">Maanc</h1>
+            <h1>Technology</h1>
+          </div>
+
+          <div className="text-[24px] md:text-[30px] lg:text-[34px] font-bold">
+            <h2>IT Consulting &</h2>
+            <h2>Software Solutions</h2>
+          </div>
+
+          <p className="text-[16px] md:text-[18px] lg:text-[19px] leading-relaxed">
+            Maanc Technologies is dedicated to helping businesses unlock their
+            full potential with expert IT consulting and innovative software
+            solutions. We partner with clients to streamline processes, boost
+            efficiency, and accelerate growth.
+          </p>
+
+          <div>
+            <Button
+              className="bg-btncolor rounded-md text-white py-2 text-[16px] md:text-[18px] lg:text-[20px]"
+              as={Link}
+              href="/contactUs"
+            >
+              Let&apos;s Talk
+            </Button>
+          </div>
+    </div>
+    <div className="w-1/2 flex items-center justify-center">
+    <Image
+            className="object-contain"
+            src="/Maancnewlogo.png"
+            alt="maanc logo"
+            width={250} // Adjusted for mobile
+            height={224} // Adjusted for mobile
+            sizes="(max-width: 768px) 150px, 
+                   (max-width: 1024px) 200px, 
+                   250px" // Automatically adjust size
+          />
+    </div>
+  </div>
+</div>
     </>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="">
+        <div className="absolute w-full flex justify-center">
           <NavMenu/>
         </div>
         {children}
