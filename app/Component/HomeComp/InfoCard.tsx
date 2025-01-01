80b62920d3c6a161aaddeb823c13a32641662275
Mohamed Rizwan <mynameisrizwan35@gmail.com>
2025-01-01 13:15:20 +0530

    added framer motion

--- app/Component/HomeComp/InfoCard.tsx
+++ app/Component/HomeComp/InfoCard.tsx
@@ -1,11 +1,15 @@
 "use client";
 import React from "react";
-
+import { motion } from "motion/react"
 const InfoCards = () => {
   return (
     <>
     <div className="h-[100svh] w-full bg-black">
-      
+    <motion.div
+            style={{ height: "500px", width: "500px", backgroundColor: "white" }}
+            animate={{ rotate: 360 }}
+            transition={{ duration: 1 }}
+        />
     </div>
     </>
   );
