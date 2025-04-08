359214341ecbeaa9bc5d4e14a0e9d1ea4af7c495
nirala7765 <kravikant249@gmail.com>
2025-04-08 23:52:19 +0530

    About page update

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import React from 'react'
 import StorySection from '../Component/AbouComp/StorySection'
+import CardSwitcher from '../Component/AbouComp/CardSwitcher'
 
 const About = () => {
   return (
     <>
     <StorySection/>
+    <CardSwitcher/>
     
     </>
   )
